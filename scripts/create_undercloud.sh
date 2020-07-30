55016224904ce8e8d5e9b159dc4ed90428562bd6
Bart van den Heuvel <bvandenh@bvandenh.remote.csb>
2020-07-30 09:57:18 +0200

    updated undercloud vm creation

--- scripts/create_undercloud.sh
+++ scripts/create_undercloud.sh
@@ -1,14 +1,53 @@
 #!/bin/bash
-virsh destroy director
-virsh undefine director
+VIRT_HOSTNAME=director
+IMAGES_DIR=/local/iso
+VIRT_DIR=/local/virt
+OFFICIAL_IMAGE=rhel-8.1-x86_64-kvm.qcow2
+PASSWORD_FOR_VMS='r3dh4t1!'
+VIRT_DOMAIN='zokahn.local'
 
-virt-install --name director --ram 8192 \
---disk path=/local/virt-machines/director.dsk,size=50 \
---vcpus 4 --os-type linux --os-variant rhel7 \
---network network=default \
---network network=deployment \
---network network=external \
---graphics none  --console pty,target_type=serial \
---location '/local/iso/rhel-server-7.6-x86_64-dvd.iso' \
---initrd-inject '/local/kickstarts/director.ks' --extra-args 'console=ttyS0,115200n8 serial' \
---extra-args 'ks=file:/director.ks' --accelerate
+virsh destroy $VIRT_HOSTNAME  > /dev/null 2>&1
+virsh undefine $VIRT_HOSTNAME > /dev/null 2>&1
+rm -f ${VIRT_DIR}/$VIRT_HOSTNAME.qcow2 > /dev/null 2>&1
+
+
+cd $VIRT_DIR
+qemu-img create -f qcow2 $VIRT_HOSTNAME.qcow2 50G
+virt-resize --expand /dev/sda1 /$IMAGES_DIR/$OFFICIAL_IMAGE $VIRT_HOSTNAME.qcow2
+
+cat > /tmp/ifcfg-eth0 << EOF
+DEVICE=eth0
+BOOTPROTO=dhcp
+ONBOOT=yes
+MTU=1500
+EOF
+
+cat > /tmp/ifcfg-eth1 << EOF
+DEVICE=eth1
+BOOTPROTO=static
+IPADDR=172.20.0.100
+PREFIX=24
+ONBOOT=yes
+MTU=1500
+EOF
+
+
+virt-customize -a $VIRT_HOSTNAME.qcow2 \
+  --hostname $VIRT_HOSTNAME.zokahn.local \
+  --root-password password:r3dh4t1! \
+  --uninstall cloud-init \
+  --copy-in /tmp/ifcfg-eth0:/etc/sysconfig/network-scripts/ \
+  --copy-in /tmp/ifcfg-eth1:/etc/sysconfig/network-scripts/ \
+  --timezone Europe/Amsterdam \
+  --selinux-relabel
+
+virt-install --ram 8192 --vcpus 2 --os-variant rhel8.0 --accelerate\
+  --disk path=$VIRT_DIR/$VIRT_HOSTNAME.qcow2,device=disk,bus=virtio,format=qcow2 \
+  --import --graphics none  --console pty,target_type=serial --graphics=vnc\
+  --network bridge=br0_1,model=virtio --name $VIRT_HOSTNAME \
+  --network bridge=br1_100,model=virtio \
+  --cpu host,+vmx --extra-args 'console=ttyS0,115200n8 serial'\
+  --dry-run --print-xml > /tmp/$VIRT_HOSTNAME.xml
+
+virsh define --file /tmp/$VIRT_HOSTNAME.xml
+virsh start $VIRT_HOSTNAME
