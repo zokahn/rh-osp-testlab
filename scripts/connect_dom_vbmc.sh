42351acad249f3807c35700d2fb0ab83cb6d2ab2
Bart van den Heuvel <bvandenh@bvandenh.remote.csb>
2020-08-03 10:24:17 +0200

    updated vmbc bind address

--- scripts/connect_dom_vbmc.sh
+++ scripts/connect_dom_vbmc.sh
@@ -6,10 +6,11 @@ binpath="/opt/vbmc/bin"
 vbmc_port=6230
 ipmi_username=admin
 ipmi_password=Wond3rfulWorld
-impi_bindaddr=192.168.122.1
+impi_bindaddr=192.168.178.113
 
 for server in $servers; do
     echo "Kicking $server into gear in VirtualBMC with port $vbmc_port"
+    $binpath/vbmc delete $server
     $binpath/vbmc add $server --address $impi_bindaddr --port $vbmc_port --username $ipmi_username --password $ipmi_password
     $binpath/vbmc start $server
     ((vbmc_port=vbmc_port+1))
