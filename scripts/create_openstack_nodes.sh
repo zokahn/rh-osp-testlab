631890191b7eefed68e78e0c2fd3baab2ff1fb11
Bart van den Heuvel <bvandenh@bvandenh.remote.csb>
2020-08-03 15:53:09 +0200

    openstack nodes need 4096MB to pass introspection!

--- scripts/create_openstack_nodes.sh
+++ scripts/create_openstack_nodes.sh
@@ -23,7 +23,7 @@ for node in $nodes; do
     echo "Kicking $node into gear"
     virsh destroy $node
     virsh undefine $node
-    virt-install --name=$node --ram=2048 --vcpus=2 \
+    virt-install --name=$node --ram=4096 --vcpus=2 \
                 --disk path=$VIRT_DIR/$node.dsk,size=100,bus=virtio \
                 --pxe --noautoconsole --graphics=vnc --hvm \
                 --network bridge=br1_100,model=virtio \
