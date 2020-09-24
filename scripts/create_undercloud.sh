09fd819969d9c365d44a251c46aee59d16824653
Bart van den Heuvel <bvandenh@bvandenh.remote.csb>
2020-09-24 17:29:57 +0200

    fixed script error about second disk

--- scripts/create_undercloud.sh
+++ scripts/create_undercloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VIRT_HOSTNAME=director
-IMAGES_DIR=/local/iso
+IMAGES_DIR=/mnt/utility/iso
 VIRT_DIR=/local/virt
 OFFICIAL_IMAGE=rhel-8.2-x86_64-kvm.qcow2
 PASSWORD_FOR_VMS='r3dh4t1!'
@@ -13,11 +13,8 @@ rm -f ${VIRT_DIR}/$VIRT_HOSTNAME.qcow2 > /dev/null 2>&1
 
 cd $VIRT_DIR
 #create root disk
-qemu-img create -f qcow2 $VIRT_HOSTNAME.qcow2 100G
+qemu-img create -f qcow2 $VIRT_HOSTNAME.qcow2 50G
 virt-resize --expand /dev/sda1 /$IMAGES_DIR/$OFFICIAL_IMAGE $VIRT_HOSTNAME.qcow2
-#create data disk
-qemu-img create -f qcow2 $VIRT_HOSTNAME-data.qcow2 100G
-
 
 
 cat > /tmp/ifcfg-eth0 << EOF
@@ -48,7 +45,6 @@ virt-customize -a $VIRT_HOSTNAME.qcow2 \
 
 virt-install --ram 8192 --vcpus 2 --os-variant rhel8.0 --accelerate \
   --disk path=$VIRT_DIR/$VIRT_HOSTNAME.qcow2,device=disk,bus=virtio,format=qcow2 \
-  --disk path=$VIRT_DIR/$VIRT_HOSTNAME-data.qcow2,device=disk,bus=virtio,format=qcow2 \
   --import --graphics none  --console pty,target_type=serial --graphics=vnc \
   --network bridge=br0_1,model=virtio --name $VIRT_HOSTNAME \
   --network bridge=br1_100,model=virtio \
