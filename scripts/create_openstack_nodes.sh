23e771937ddd810136e8aaee9c4f866d3f42fa80
Bart van den Heuvel <bart@redhat.com>
2020-10-01 19:19:19 +0200

    added 20g more memory to controllers

--- scripts/create_openstack_nodes.sh
+++ scripts/create_openstack_nodes.sh
@@ -10,7 +10,7 @@ for node in $nodes; do
     echo "Kicking $node into gear"
     virsh destroy $node
     virsh undefine $node
-    virt-install --name=$node --ram=6144 --vcpus=2 \
+    virt-install --name=$node --ram=24576 --vcpus=2 \
                 --disk path=$VIRT_DIR/$node.dsk,size=100,bus=virtio \
                 --pxe --noautoconsole --graphics=vnc --hvm \
                 --network bridge=br1_100,model=virtio,mac=52:54:00:"$NUM"5:bd:2f \
