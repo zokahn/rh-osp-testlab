fbe9d4a2ad3ae3c1153c4eb9bc82f6da71f5f464
Bart van den Heuvel <bvandenh@redhat.com>
2021-06-09 13:43:17 +0200

    changed to libvirt networking

--- scripts/create_ceph_nodes.sh
+++ scripts/create_ceph_nodes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 VIRT_DIR=/local/virt
 
-VIRT_DOMAIN='zokahn.local'
+VIRT_DOMAIN='simpletest.nl'
 
 
 nodes="ceph1 ceph2 ceph3"
@@ -16,8 +16,9 @@ for node in $nodes; do
                 --disk path=$VIRT_DIR/$node.dsk,size=100,bus=virtio \
                 --disk path=$VIRT_DIR/$node-data.dsk,size=50,bus=virtio \
                 --pxe --noautoconsole --graphics=vnc --hvm \
-                --network bridge=br1_100,model=virtio,mac=52:54:00:"$NUM"5:bd:2f \
-                --network bridge=br1_100,model=virtio \
-                --os-variant=rhel8.0
+                --network network=deployment,model=virtio,mac=52:54:00:"$NUM"5:bd:2f \
+                --network network=storage,model=virtio \
+                --network network=storagemgmt,model=virtio \
+                --os-variant=rhel7.0
     NUM=$((NUM+1))
 done
